11de5a08f6e69b2fe884d146aba5555f321776c0
bugwithabackpack <bugwithabackpack@gmail.com>
2024-02-19 23:05:32 +0530

    did something

--- bug.js
+++ bug.js
@@ -1,8 +1,11 @@
-// Get the button element
-var button = document.getElementById("redirectButton");
+// Add event listener for DOMContentLoaded event
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the button element
+    var button = document.getElementById("redirectButton");
 
-// Add event listener for button click
-button.addEventListener("click", function() {
-    // Redirect to another page
-    window.location.href = "MyClicks.html";
+    // Add event listener for button click
+    button.addEventListener("click", function() {
+        // Redirect to another page
+        window.location.href = "MyClicks.html";
+    });
 });
