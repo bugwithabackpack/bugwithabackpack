f34fc04e79f61db872b7d7a07c067e1d4e52c0a9
bugwithabackpack <bugwithabackpack@gmail.com>
2024-02-20 16:11:57 +0530

    added about me

--- bug.js
+++ bug.js
@@ -1,23 +1,18 @@
 // Add event listener for DOMContentLoaded event
 document.addEventListener("DOMContentLoaded", function() {
     // Get the button element
-    var button = document.getElementById("redirectButton");
+    var redirectButton = document.getElementById("redirectButton");
+    var aboutMeButton = document.getElementById("aboutme");
 
-    // Add event listener for button click
-    button.addEventListener("click", function() {
-        // Redirect to another page
+    // Add event listener for button click (redirectButton)
+    redirectButton.addEventListener("click", function() {
+        // Redirect to MyClicks.html
         window.location.href = "MyClicks.html";
     });
-});
-// Add event listener for DOMContentLoaded event
-document.addEventListener("DOMContentLoaded", function() {
-    // Get the button element
-    var button = document.getElementById("aboutme");
 
-    // Add event listener for button click
-    button.addEventListener("click", function() {
-        // Redirect to another page
+    // Add event listener for button click (aboutMeButton)
+    aboutMeButton.addEventListener("click", function() {
+        // Redirect to aboutme.html
         window.location.href = "aboutme.html";
     });
 });
-
